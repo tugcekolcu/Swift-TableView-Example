6c6c64f0fc64c7c73454d14cc190cc95a228faab
Tuğçe Kolcu <tugce.kolcu@akbank.com>
2019-01-31 21:59:16 +0300

    Initial Commit

---
+++ Landmark Book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Landmark Book
+//
+//  Created by Tuğçe Kolcu on 31.01.2019.
+//  Copyright © 2019 Tuğçe Kolcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
